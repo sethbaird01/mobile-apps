20df0efda1caf060ede7be56eb0b8e949295efb0
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-09-28 08:50:32 -0500

    IP04 Initial commit

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Baird, Seth J on 9/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+    }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+           if motion == .motionShake {
+             //some hilarious code
+        }
+        }
+
+}
+
