66723af47c6d17d38c337981dc6b502f97de1962
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-09-30 09:51:37 -0500

    IP04 Complete

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -9,16 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //outlet declaration
+    @IBOutlet weak var labelOut: UILabel!
+    @IBOutlet weak var thinkOut: UILabel!
+    @IBOutlet weak var imageOut: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
     }
-
+    
+    let responses: [String] = ["Yes","No","Maybe so", "Certainly", "Without a doubt", "Doubtful"]
+    //empty label state
+    let empty: String = "                                                 "
+    
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
            if motion == .motionShake {
-             //some hilarious code
-        }
-        }
+               //add text
+               labelOut.text = responses[Int.random(in: 0...responses.count-1)]
+               print("text: \(labelOut.text ?? "unknown")")
 
-}
+               //fade into new image
+               UIView.transition(with: self.imageOut, duration:1,
+                 options: UIView.AnimationOptions.transitionCrossDissolve, animations: { self.imageOut.image = UIImage.init(named: "8blue") })
+               
+               //fade labels
+               UIView.transition(with: self.thinkOut, duration:1,
+                                 options: UIView.AnimationOptions.transitionCrossDissolve, animations: { self.thinkOut.alpha = 0 })
+               UIView.transition(with: self.labelOut, duration:1,
+                                 options: UIView.AnimationOptions.transitionCrossDissolve, animations: { self.labelOut.alpha = 1 })
+               
+               //reset timer
+               timer = Timer.scheduledTimer(withTimeInterval: 4.0, repeats: true) { timer in
+                   self.resetUI()
+               }
+           }
+        }
+    
+    //public scope
+    var timer: Timer = Timer()
+    
+    //bring back faded elements
+    func resetUI(){
+        //image fade
+        UIView.transition(with: self.imageOut, duration:1,
+          options: UIView.AnimationOptions.transitionCrossDissolve, animations: { self.imageOut.image = UIImage.init(named: "8square") })
+        
+        //text fade
+        UIView.transition(with: self.thinkOut, duration:1,
+                          options: UIView.AnimationOptions.transitionCrossDissolve, animations: { self.thinkOut.alpha = 1 })
+        UIView.transition(with: self.labelOut, duration:1,
+                          options: UIView.AnimationOptions.transitionCrossDissolve, animations: { self.labelOut.alpha = 0 })
+        //remove text while its invisible
+        labelOut.text = empty
+        
+        //remove timer so it doesnt keep running
+        timer.invalidate()
+    }
 
+}//
