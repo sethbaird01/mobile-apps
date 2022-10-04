b241b218d82ea664953f92162de68b35f69a5c07
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-10-04 10:13:07 -0500

    add labels

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var outletCollection: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        outletCollection[0].titleLabel?.text = "testOne"
     }
 
 
