e17891bfc412ff22aa558d25dc26ec033a6231a6
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-10-04 09:22:43 -0500

    IP05 init

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Baird, Seth J on 10/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
