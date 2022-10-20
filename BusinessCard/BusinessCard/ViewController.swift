bb487301dc227626f3ba2db540f29fa9ce18bf1f
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-10-20 08:48:03 -0500

    IP06 template

---
+++ BusinessCard/BusinessCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusinessCard
+//
+//  Created by Baird, Seth J on 10/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
