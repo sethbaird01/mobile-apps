19aa1f3ad8cc7a0259b946a0e607817059f423d6
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-10-26 08:59:53 -0500

    Book 2.10

---
+++ CommonInputControls/CommonInputControls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommonInputControls
+//
+//  Created by Baird, Seth J on 10/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
