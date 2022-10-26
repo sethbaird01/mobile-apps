a3a163d4ca86d81a44c11ef4db17ae70e6306f1f
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-10-26 09:30:52 -0500

    Book 2.10 Complete

--- CommonInputControls/CommonInputControls/ViewController.swift
+++ CommonInputControls/CommonInputControls/ViewController.swift
@@ -8,7 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //not organizing this
+    @IBAction func gestureTap(_ sender: UIGestureRecognizer) {
+        print(sender.location(in: view))
+    }
+    @IBOutlet weak var label: UILabel!
+    @IBAction func `switch`(_ sender: UISwitch) {
+        if sender.isOn {
+                print("switch is on")
+            } else {
+                print("switch is off")
+            }
 
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("button")
+    }
+    
+    @IBAction func slider(_ sender: UISlider) {
+        print(sender.value)
+    }
+    @IBAction func textReturn(_ sender: UITextField) {
+        print(sender.text!)
+        label.text = sender.text
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
