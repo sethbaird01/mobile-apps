e8323cbc3be6f03290fd711ffcabe7a8a7dab5c8
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-10-28 08:51:41 -0500

    IP07 template

---
+++ RGBMatcher/RGBMatcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBMatcher
+//
+//  Created by Baird, Seth J on 10/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
