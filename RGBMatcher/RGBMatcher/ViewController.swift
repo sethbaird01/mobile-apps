cef80daeddd1e15fa659e43b2e1b81e1c228e565
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-11-01 09:25:12 -0500

    IP07 Complete

--- RGBMatcher/RGBMatcher/ViewController.swift
+++ RGBMatcher/RGBMatcher/ViewController.swift
@@ -22,8 +22,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initUI()
-        
     }
+    
     func initUI() {
         //border views
         leftView.layer.borderWidth = 2
@@ -32,19 +32,25 @@ class ViewController: UIViewController {
         //reset slider set value
         for slider in sliderCollection {
             slider.value = 1.0;
-            rightView.backgroundColor = .white
         }
         
-        //generate color
+        //generate and set colors
+        userColor = .white
         goalColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
         
         //set view color
+        leftView.backgroundColor = userColor
         rightView.backgroundColor = goalColor
+        
+        //label text
+        timeLabel.text = "Timer paused"
     }
     
     @IBAction func sliderActions(_ sender: UISlider) {
         if(!gameStarted){
+            //start game and make new timer
             gameStarted = true
+            time = 8
             timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
                 self.tick()}
         }
@@ -71,8 +77,12 @@ class ViewController: UIViewController {
     }
     
     func tick() {
+//        print("tick")
+        //decrement time and update label every 1s
         time -= 1
         timeLabel.text = "Time left: \(time)"
+        
+        //end game when time out
         if(time == 0){
             endGame()
         }
@@ -88,18 +98,16 @@ class ViewController: UIViewController {
         var userRed = CGFloat(), userGreen = CGFloat(), userBlue = CGFloat(), goalRed = CGFloat(), goalGreen = CGFloat(), goalBlue = CGFloat()
         userColor.getRed(&userRed, green: &userGreen, blue: &userBlue, alpha: nil)
         goalColor.getRed(&goalRed, green: &goalGreen, blue: &goalBlue, alpha: nil)
-        var score = Int((1 - sqrt(pow(userRed-goalRed, 2) + pow(userGreen-goalGreen, 2) + pow(userBlue-goalBlue, 2)))*100)
+        let score = Int((1 - sqrt(pow(userRed-goalRed, 2) + pow(userGreen-goalGreen, 2) + pow(userBlue-goalBlue, 2)))*100)
         
         //create and present alert
-        var dialogMessage = UIAlertController(title: "Time Up", message: "Score: \(score)", preferredStyle: .alert)
+        let dialogMessage = UIAlertController(title: "Time Up", message: "Score: \(score)", preferredStyle: .alert)
         let ok = UIAlertAction(title: "Reset Game", style: .default, handler: { (action) -> Void in
             self.initUI()
          })
         dialogMessage.addAction(ok)
         self.present(dialogMessage, animated: true, completion: nil)
         
-        //initui
-        initUI()
     }
 }
 
