6cc4393380a03754b059388e451e8a6795ec8263
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-11-07 09:25:21 -0600

    IP08 complete

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -12,20 +12,33 @@ class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
 
+    @IBAction func playSound(_ sender: UIButton) {
+            var note = ""
+        switch sender.tag {
+        case 0:
+            note = "A"
+        case 1:
+            note = "B"
+        case 2:
+            note = "C"
+        case 3:
+            note = "D"
+        case 4:
+            note = "E"
+        case 5:
+            note = "F"
+        case 6:
+            note = "G"
+        default:
+            print("tag exception")
+        }
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+                player = try! AVAudioPlayer(contentsOf: url!)
+                player.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
-    }
-    
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player.play()
-                
-    }
 }
-
-
