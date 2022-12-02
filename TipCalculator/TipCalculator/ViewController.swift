7fb4947f71aa32b0de2f7e744d3526bf995fbbd6
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-12-02 08:52:34 -0600

    IP09 template

---
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Baird, Seth J on 12/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
