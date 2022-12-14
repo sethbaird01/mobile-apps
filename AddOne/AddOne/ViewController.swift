77a69ec73607e53bb0a6b53fe71c170e68ef6215
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-12-14 10:14:25 -0600

    IP10 Initial commit

---
+++ AddOne/AddOne/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AddOne
+//
+//  Created by Baird, Seth J on 12/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var moneyLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var centerLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
