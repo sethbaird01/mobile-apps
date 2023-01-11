8584aec1c0cc94f03305014e64a3745da5430198
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2023-01-11 10:13:33 -0600

    Login initial commit

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Baird, Seth J on 1/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
