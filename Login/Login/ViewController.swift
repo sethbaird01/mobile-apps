c436dcf3a96816b4cab4198fe7cfebdce5a58983
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2023-01-24 10:13:14 -0600

    Login Complete

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -13,7 +13,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var username: String = ""
+    var password: String = ""
 
-
+    
+    @IBAction func usernameField(_ sender: UITextField) {
+        username = sender.text ?? "Home"
+    }
+    
+    @IBAction func passField(_ sender: UITextField) {
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "login", sender: sender)
+    }
+    
+    @IBAction func forgotUserBtn(_ sender: UIButton) {
+        performSegue(withIdentifier: "forgotUserPass", sender: sender)
+    }
+    
+    @IBAction func forgotPassBtn(_ sender: UIButton) {
+        performSegue(withIdentifier: "forgotUserPass", sender: sender)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "login"){
+            segue.destination.title = username
+        }else{
+            segue.destination.title = (sender as! UIButton).titleLabel?.text ?? "Default Segue"
+        }
+    }
 }
 
