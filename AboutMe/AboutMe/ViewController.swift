22e803e0ee5e6b14c12f2f2840e042a7d0df6408
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2023-02-21 09:20:42 -0600

    AboutMe

---
+++ AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Baird, Seth J on 2/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
