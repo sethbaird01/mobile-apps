2508959d1b0e88a14a64abd65bb5c9828158b87b
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2023-03-22 10:14:29 -0500

    IP13 Initial Commit

---
+++ Captcha/Captcha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Captcha
+//
+//  Created by Baird, Seth J on 3/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+}
+
