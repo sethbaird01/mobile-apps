3f486d63fb2b4ecf975273c729182ae4aa53d14f
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2023-03-28 10:14:52 -0500

    IP13 Incremental Commit

--- Captcha/Captcha/AudioViewController.swift
+++ Captcha/Captcha/AudioViewController.swift
@@ -9,28 +9,58 @@ import UIKit
 
 class AudioViewController: UIViewController {
 
+    //gen correct captcha
+    var correctIdx = 100
+    var correctWord = ""
+    var correctPath = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
 
-        let soundsPath = "\(path)+/Sounds"
-        let soundsList =  try! fm.contentsOfDirectory(atPath: soundsPath)
+        let audioPath = path+"/Sounds/"
+        var audioList = try! fm.contentsOfDirectory(atPath: audioPath)
         
-
+        var chosenAudio = Array(Set(Array(repeating: audioList.randomElement(), count: 4)))
         
-        //set labels to na
+        while chosenAudio.count < 4 {
+            chosenAudio.append(audioList.randomElement())
+            //remove duplicates
+            chosenAudio = Array(Set(chosenAudio))
+        }
+        
+        correctIdx = Int.random(in: 0...3)
+        correctPath = chosenAudio[correctIdx]!!
+        correctWord = chosenAudio[correctIdx]!.components(separatedBy: "_")[0]
 
+//        //set labels text
+        var idx = 0;
+        for curLabel in textCollection {
+            //remove trailing text
+            curLabel.text = chosenAudio[idx]!.components(separatedBy: "_")[0]
+            idx += 1
+        }
+        
     }
     
 
     
     @IBAction func labelButtons(_ sender: UITapGestureRecognizer) {
-        var selectedTextIndex = sender.view!.tag - 5
+        print("gesture")
+        if((sender.delegate as! CaptchaLabel).text == correctWord){
+            print("segue")
+            performSegue(withIdentifier: "complete", sender: self)
+        }else{
+            (sender.delegate as! CaptchaImageView).shake()
+        }
     }
     @IBOutlet var textCollection: [UILabel]!
     
     @IBAction func playButton(_ sender: Any) {
+        let url = Bundle.main.url(forResource: chose, withExtension: "wav")
+                player = try! AVAudioPlayer(contentsOf: url!)
+                player.play()
     }
 }
--- Captcha/Captcha/ImageViewController.swift
+++ Captcha/Captcha/ImageViewController.swift
@@ -9,15 +9,24 @@ import UIKit
 
 class ImageViewController: UIViewController {
     
+    
+    //gen correct captcha
+    var correctIdx = Int.random(in: 0...3)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        CIViews[0].isUserInteractionEnabled = true
+        
+        
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
 
-        let imagesPath = "\(path)+/Images"
+        let imagesPath = path+"/Images/"
         var imagesList = try! fm.contentsOfDirectory(atPath: imagesPath)
         
-        var chosenImages = Array(repeating: imagesList.randomElement(), count: 4)
+        var chosenImages = Array(Set(Array(repeating: imagesList.randomElement(), count: 4)))
         
         while chosenImages.count < 4 {
             chosenImages.append(imagesList.randomElement())
@@ -28,14 +37,23 @@ class ImageViewController: UIViewController {
         //set images
         var idx = 0;
         for capImage in CIViews {
-            capImage.image = UIImage(contentsOfFile: (URL(string: imagesPath)?.appending(path: chosenImages[idx]!))!)
-            idx++
+            capImage.image = UIImage(contentsOfFile: (imagesPath+chosenImages[idx]!))
+            idx += 1
         }
+        
+        //remove extension
+        textLabel.text = chosenImages[correctIdx]!.components(separatedBy: ".")[0]
       
     }
 
-    @IBAction func imageButtons(_ sender: Any) {
-        
+    @IBAction func imageButtons(_ sender: UITapGestureRecognizer) {
+        print("gesture")
+        if((sender.delegate as! CaptchaImageView).tag - 1 == correctIdx){
+            print("segue")
+            performSegue(withIdentifier: "audio", sender: self)
+        }else{
+            (sender.delegate as! CaptchaImageView).shake()
+        }
     }
     @IBOutlet var CIViews: [CaptchaImageView]!
     @IBOutlet weak var textLabel: UILabel!
