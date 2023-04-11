922ac92705752b54ffb3c564de2da433a1605bf9
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2023-04-11 10:14:33 -0500

    IP14 Initial Commit

---
+++ Wordle/Wordle/Model/Wordle.swift
@@ -0,0 +1,8 @@
+//
+//  Wordle.swift
+//  Wordle
+//
+//  Created by Baird, Seth J on 4/11/23.
+//
+
+import Foundation
---
+++ Wordle/Wordle/View/GameView.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Wordle
+//
+//  Created by Baird, Seth J on 4/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func textFieldOutlet(_ sender: Any) {
+    }
+    @IBAction func textEntry(_ sender: Any) {
+    }
+    
+}
+
